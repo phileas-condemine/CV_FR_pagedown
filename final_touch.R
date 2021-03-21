f1cf2a8b81579b7458bb8b4464e222be5732e131
Philéas Condemine <phileas.condemine@sante.gouv.fr>
2021-03-21 14:20:02 +0100

    readme can't be rmd

--- final_touch.R
+++ final_touch.R
@@ -4,7 +4,13 @@ usethis::use_mit_license("Philéas Condemine")
 rmarkdown::render_site()
 unlink("docs",T,T)
 shell("rename _site docs")# rename directory _site/ to docs
-file.copy("index.Rmd","readme.md",overwrite = T)# copy index.Rmd (CV) to readme.md
+# file.copy("index.Rmd","readme.md",overwrite = T)# copy index.Rmd (CV) to readme.md
 file.rename("index.pdf","cv_fr_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
 
 
+system('git config --global user.email "phileas.condemine@gmail.com"')
+system('git config --global user.name "Phileas Condemine"')
+
+system("git add .")
+system(sprintf('git commit -m "update %s"',Sys.Date()))
+system("git push origin master")
