6ee3f10d6d5b686be874cfd42d35e8141e293136
Phileas Condemine <phileas.condemine@gmail.com>
2022-02-28 21:54:41 +0100

    update 2022-02-28

--- final_touch.R
+++ final_touch.R
@@ -1,11 +1,10 @@
 usethis::use_mit_license("Philéas Condemine")
 
-
-rmarkdown::render_site()
 unlink("docs",T,T)
-shell("rename _site docs")# rename directory _site/ to docs
+rmarkdown::render_site()
+# shell("rename _site docs")# rename directory _site/ to docs
 # file.copy("index.Rmd","readme.md",overwrite = T)# copy index.Rmd (CV) to readme.md
-file.rename("index.pdf","cv_fr_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
+# file.rename("index.pdf","cv_fr_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
 file.copy("docs/cv_fr_Phileas_Condemine.pdf","cv_fr_Phileas_Condemine.pdf",overwrite = T)
 
 system('git config --global user.email "phileas.condemine@gmail.com"')
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
+---
+title: "CV Phileas Condemine"
+description: "Page to handle french and english versions of the resume"
+---
+
 [The online resume is HERE](https://phileas-condemine.github.io/CV_EN_pagedown/)
 
 [Le CV se trouve ici](https://phileas-condemine.github.io/CV_FR_pagedown/)
