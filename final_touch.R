ffec137af0d2060a3dee765af13ff34132eaa007
Phileas Condemine <phileas.condemine@gmail.com>
2022-11-16 19:22:25 +0100

    update 2022-11-16

--- final_touch.R
+++ final_touch.R
@@ -1,10 +1,12 @@
 usethis::use_mit_license("Philéas Condemine")
 
-unlink("docs",T,T)
+
+# unlink("docs",T,T)
+rmarkdown::clean_site(preview = FALSE)
+
 rmarkdown::render_site()
-# shell("rename _site docs")# rename directory _site/ to docs
-# file.copy("index.Rmd","readme.md",overwrite = T)# copy index.Rmd (CV) to readme.md
-# file.rename("index.pdf","cv_fr_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
+
+file.rename("docs/index.pdf","docs/cv_fr_Phileas_Condemine.pdf")# copy index.Rmd (CV) to readme.md
 file.copy("docs/cv_fr_Phileas_Condemine.pdf","cv_fr_Phileas_Condemine.pdf",overwrite = T)
 
 system('git config --global user.email "phileas.condemine@gmail.com"')
@@ -13,3 +15,9 @@ system('git config --global user.name "Phileas Condemine"')
 system("git add .")
 system(sprintf('git commit -m "update %s"',Sys.Date()))
 system("git push origin master")
+
+print("
+to publish the website on netim (phileascondemine.com),
+open filezilla, connect to the hosting server,
+copy the docs/ files in the public_www/ folder on the remote.
+      ")
